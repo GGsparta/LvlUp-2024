a145f869c238a1d4b351195169f47f2497c4e35c
Yoann Haffner <yoyofrom68@gmail.com>
2024-02-17 15:20:55 +0100

    Player baique en 2D

---
+++ Assets/CraftemIpsum/Scripts/2D/Planet.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CraftemIpsum._2D
+{
+    public class Planet : MonoBehaviour
+    {
+        [SerializeField] private float gravityScale;
+        [SerializeField] private float gravityDamping;
+        [SerializeField] private CircleCollider2D gravityZone;
+
+        private readonly List<Rigidbody2D> _listOfAttractedBodies = new();
+
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.TryGetComponent(out Rigidbody2D body))
+                _listOfAttractedBodies.Add(body);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if(other.TryGetComponent(out Rigidbody2D body))
+                _listOfAttractedBodies.Remove(body);
+        }
+
+        public void FixedUpdate()
+        {
+            foreach (Rigidbody2D body in _listOfAttractedBodies)
+            {
+                Vector3 offset = body.transform.position - transform.position;
+                float damping = 1 - (offset.magnitude / (gravityZone.radius * transform.lossyScale.magnitude) * gravityDamping);
+
+                body.AddForce(-offset.normalized * (gravityScale * body.mass * damping), ForceMode2D.Force);
+            }
+        }
+    }
+}
---
+++ Assets/CraftemIpsum/Scripts/2D/Player.cs
@@ -0,0 +1,104 @@
+using System;
+using GGL.Extensions;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+namespace CraftemIpsum._2D
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float speed = 2f;
+        [SerializeField] private float jumpForce = 3f;
+        [SerializeField] private float maxSpeed = 5f;
+        [SerializeField] private float groundedDamping = 0;
+
+
+        private InputAction _walk, _jump, _pickUp;
+        private Rigidbody2D _rigidbody;
+        private bool _grounded;
+        
+        public bool CanMove => true;
+
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Ground")) _grounded = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Ground")) _grounded = false;
+        }
+
+        private void Awake()
+        {
+            PlayerInput input = GetComponent<PlayerInput>();
+            _walk = input.actions["Walk"];
+            _pickUp = input.actions["PickUp"];
+            _jump = input.actions["Jump"];
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _grounded = true;
+
+            _jump.performed += DoJump;
+        }
+
+        private void OnEnable()
+        {
+            _walk.Enable();
+            _pickUp.Enable();
+            _jump.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _walk.Disable();
+            _pickUp.Disable();
+            _jump.Disable();
+        }
+
+        public void Update()
+        {
+            if (CanMove)
+            {
+                DoWalk();
+                //DoJump();
+                DoPickUp();
+            }
+
+            UpdateGraphics();
+        }
+
+        private void DoPickUp()
+        {
+            // todo
+        }
+
+        private void DoJump(InputAction.CallbackContext callbackContext)
+        {
+            if (!_grounded)
+                return;
+
+            _rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            _grounded = false;
+        }
+
+        private void DoWalk()
+        {
+
+            if (!_grounded)
+                return;
+
+            float input = _walk.ReadValue<float>();
+            float currentSpeed = speed;
+
+            _rigidbody.AddForce(transform.right * (input * currentSpeed), ForceMode2D.Impulse);
+            _rigidbody.velocity = _rigidbody.velocity.normalized * (Mathf.Min(_rigidbody.velocity.magnitude, maxSpeed) * (1 - groundedDamping));
+        }
+
+        private void UpdateGraphics()
+        {
+            transform.up = transform.position.normalized;
+        }
+    }
+}
