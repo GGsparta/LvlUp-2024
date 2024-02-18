51ee62d27a0775c20ea6efce8302397d25df26a7
Yoann Haffner <yoyofrom68@gmail.com>
2024-02-18 11:55:07 +0100

    Boucle de jeu fonctionnellle

---
+++ Assets/CraftemIpsum/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Diagnostics;
+using GGL.Singleton;
+using UnityEngine;
+
+namespace CraftemIpsum
+{
+    public class GameManager : CachedBehaviour<GameManager>
+    {
+        [SerializeField] private int durationInSeconds = 120;
+        [SerializeField] private float delayBeforeStart = 4;
+
+        private Stopwatch _stopwatch;
+
+        public event Action OnPlay;
+        public event Action OnPause;
+
+        public bool IsPlaying { get; private set; } = false;
+        public int Score { get; private set; }
+        public int SecondsLeft { get; private set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _stopwatch = new Stopwatch();
+            SecondsLeft = durationInSeconds;
+            Invoke(nameof(Play), delayBeforeStart);
+        }
+
+        private void Update()
+        {
+            SecondsLeft = durationInSeconds - (int)_stopwatch.Elapsed.TotalSeconds;
+        }
+
+        public void Play()
+        {
+            IsPlaying = true;
+            _stopwatch.Start();
+            OnPlay?.Invoke();
+        }
+
+        public void Pause()
+        {
+            IsPlaying = false;
+            _stopwatch.Stop();
+            OnPause?.Invoke();
+        }
+
+        public void IncrementScore()
+        {
+            Score++;
+        }
+    }
+}
---
+++ Assets/CraftemIpsum/Scripts/HUD.cs
@@ -0,0 +1,60 @@
+using GGL.UI.Window;
+using TMPro;
+using UnityEngine;
+
+namespace CraftemIpsum
+{
+    public class HUD : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI timerDisplay;
+        [SerializeField] private TextMeshProUGUI scoreDisplay;
+        [SerializeField] private Popup gameOverPopup;
+        [SerializeField] private TextMeshProUGUI goTimerDisplay;
+        [SerializeField] private TextMeshProUGUI goScoreDisplay;
+
+
+        private int _secondsLeftBuffer = -1;
+        private int _scoreBuffer = -1;
+        
+
+        private void Update()
+        {
+            Debug.Log(GameManager.Instance.SecondsLeft);
+            if (GameManager.Instance.SecondsLeft != _secondsLeftBuffer)
+            {
+                _secondsLeftBuffer = GameManager.Instance.SecondsLeft;
+                RefreshTimer();
+
+                if (_secondsLeftBuffer <= 0)
+                {
+                    GameManager.Instance.Pause();
+                    GameOver();
+                }
+            }
+
+            
+            if (GameManager.Instance.Score != _scoreBuffer)
+            {
+                _scoreBuffer = GameManager.Instance.Score;
+                RefreshScore();
+            }
+        }
+
+        private void RefreshTimer()
+        {
+            timerDisplay.text = $"{_secondsLeftBuffer / 60}:{_secondsLeftBuffer % 60}";
+        }
+        
+        private void RefreshScore()
+        {
+            scoreDisplay.text = $"<b>{_scoreBuffer}</b><i><size=26> déchet(s) recyclé(s)</size></i>";
+        }
+
+        public void GameOver()
+        {
+            goTimerDisplay.text = timerDisplay.text;
+            goScoreDisplay.text = scoreDisplay.text;
+            gameOverPopup.Open();
+        }
+    }
+}
