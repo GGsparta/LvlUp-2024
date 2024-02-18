c65a5954e502864b386324e302df12603a43369c
Yoann Haffner <yoyofrom68@gmail.com>
2024-02-18 14:11:39 +0100

    Portal 2D lights

--- Assets/CraftemIpsum/Scripts/2D/Portal.cs
+++ Assets/CraftemIpsum/Scripts/2D/Portal.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
@@ -6,12 +7,27 @@ namespace CraftemIpsum._2D
     public class Portal : MonoBehaviour
     {
         [SerializeField] private Waste[] wastePrefabs;
+        [SerializeField] private Light spotlight;
         public PortalColor color;
 
         public void SpawnWaste(WasteType type)
         {
             GameObject go = Instantiate(wastePrefabs.First(w => w.Type == type).gameObject);
             go.transform.position = transform.position;
+
+            StopAllCoroutines();
+            StartCoroutine(EBlinkLight());
+        }
+
+        private IEnumerator EBlinkLight()
+        {
+            spotlight.enabled = true;
+            yield return new WaitForSeconds(.2f);
+            spotlight.enabled = false;
+            yield return new WaitForSeconds(.2f);
+            spotlight.enabled = true;
+            yield return new WaitForSeconds(.2f);
+            spotlight.enabled = false;
         }
     }
 }
